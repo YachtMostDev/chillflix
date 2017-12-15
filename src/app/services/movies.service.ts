a342e6bd84712c5ba21661a0f0bb291ec3e0bf17
Unknown <kayvanbree@gmail.com>
2017-12-15 12:17:49 +0100

    Fancy ass scrollbar

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -4,13 +4,15 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class MoviesService {
   private file = '../../assets/movies.json';
-  private movies;
 
   constructor(private http: HttpClient) {
-    this.movies = this.http.get(this.file);
   }
 
   getAll = () => {
+    return this.http.get(this.file);
+  }
+
+  getPopular = () => {
     return this.http.get(this.file);
   }
 }
