478b02a8a6182b081a213383fac14a5d44876861
gorgony <n.j.vannispen@gmail.com>
2018-01-27 18:07:58 +0100

    fix

--- src/app/components/film-detail/film-detail.component.ts
+++ src/app/components/film-detail/film-detail.component.ts
@@ -1,26 +1,32 @@
-import { Store } from '@ngrx/store';
-import { FilmService } from './../../services/film.service';
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Store} from '@ngrx/store';
+import {FilmService} from './../../services/film.service';
+import {Component, OnInit, Input} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/find';
 import 'rxjs/add/operator/do';
+import {DESELECT_FILM} from "../../state/films.actions";
 
 @Component({
-  selector: 'app-film-detail',
-  templateUrl: './film-detail.component.html',
-  styleUrls: ['./film-detail.component.css']
+	selector: 'app-film-detail',
+	templateUrl: './film-detail.component.html',
+	styleUrls: ['./film-detail.component.css']
 })
 export class FilmDetailComponent implements OnInit {
 
-  private film;
+	private film;
 
-  constructor(private route: ActivatedRoute, private filmService: FilmService, private store: Store<any>) { }
+	constructor(private route: ActivatedRoute, private filmService: FilmService, private store: Store<any>) {
+	}
 
-  ngOnInit() {
-  	this.filmService.getAll();
-	this.route.params.flatMap(params => {
-	  return this.store.select("films").pluck("films").find((film: any) => film.id === parseInt(params["id"], 10));
-	}).subscribe(film => this.film = film);
-  }
+	ngOnInit() {
+		this.filmService.getAll();
+		this.store.select("films").subscribe(state => this.film = state.films.find(film => film.id === state.selectedFilm));
+	}
+
+	deselect() {
+		this.store.dispatch({
+			type: DESELECT_FILM
+		});
+	}
 }
