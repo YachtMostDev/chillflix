0ccb01418041e7461f13810b6f58b9350c7fd367
Tom <tomliefheid@gmail.com>
2018-01-27 12:57:42 +0100

    added logic to button

--- src/app/components/add-movie/add-movie.component.ts
+++ src/app/components/add-movie/add-movie.component.ts
@@ -1,21 +1,38 @@
 import { Store } from '@ngrx/store';
+import { ADD_TO_QUEUE } from '../../state/films.actions';
 import { Component, OnInit } from '@angular/core';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
-  selector: 'app-add-movie',
-  templateUrl: './add-movie.component.html',
-  styleUrls: ['./add-movie.component.css']
+	selector: 'app-add-movie',
+	templateUrl: './add-movie.component.html',
+	styleUrls: ['./add-movie.component.css']
 })
-export class AddMovieComponent implements OnInit {
+export class AddMovieComponent implements OnInit, OnDestroy {
 
-  constructor(private store: Store<any>) { }
+	private selectedFilm;
+	private subscription;
+ 	constructor(private store: Store<any>) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.subscription = this.store.select('films')
+			.map(state => state.selectedFilm)
+			.subscribe(film => {
+				this.selectedFilm = film;
+			});
+	}
+	ngOnDestroy() {
+		this.subscription.unsubscribe();
+	}
 
-  addMovieClick() {
-	  // check if statemanager has selected film.
-	  // if true: call state manager to add that film to the to-watch queue
-	  console.log('clicked add movie');
-  }
+	addMovieClick() {
+		// check if statemanager has selected film.
+		// if true: call state manager to add that film to the to-watch queue
+		if (this.selectedFilm != null) {
+			console.log('adding' + this.selectedFilm.id + ' to the queue');
+			this.store.select('films').dispatch({ type: ADD_TO_QUEUE, payload: this.selectedFilm.id });
+		} else {
+			console.log('selected film = null, can\'t add to queue');
+		}
+	}
 }
