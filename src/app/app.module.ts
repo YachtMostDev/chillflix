1b69779fd7a3b756dd7faa264a51c4cd0660ccb6
Unknown <kayvanbree@gmail.com>
2018-01-29 18:50:23 +0100

    Moved some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,11 @@ import {FilmDetailComponent} from './components//film-detail/film-detail.compone
 import {AboutComponent} from './components//about/about.component';
 import {Material} from './material/material.module';
 import { FilmService } from './services/film.service';
-import { RatingComponent } from './rating/rating.component';
+import { RatingComponent } from './components/rating/rating.component';
 import { FilmsComponent } from './components/films/films.component';
 import { FilmListItemTagComponent } from './components/film-list-item-tag/film-list-item-tag.component';
 import {HttpClientModule} from '@angular/common/http';
-import {HamburgerMenuComponent} from './hamburger-menu/hamburger-menu.component';
+import {HamburgerMenuComponent} from './components/hamburger-menu/hamburger-menu.component';
 import {AddMovieComponent} from './components/add-movie/add-movie.component';
 import {AppRoutingModule} from './routers/app-routing.module';
 import {ThumbsUpComponent} from './components/thumbs-up/thumbs-up.component';
