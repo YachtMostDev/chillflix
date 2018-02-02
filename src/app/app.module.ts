6c9311e1efc89d7eeae1d6026173ef172a239de8
gorgony <n.j.vannispen@gmail.com>
2018-02-03 00:06:53 +0100

    Fixed some stuff, added the login page, added security to the routing, added a localstorage service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { filmsReducer } from './state/films.reducer';
+import {StoreModule} from '@ngrx/store';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {filmsReducer} from './state/films.reducer';
 
 import {AppComponent} from './app.component';
 
@@ -12,43 +13,49 @@ import {FilmListItemComponent} from './components//film-list-item/film-list-item
 import {FilmDetailComponent} from './components//film-detail/film-detail.component';
 import {AboutComponent} from './components//about/about.component';
 import {Material} from './material/material.module';
-import { FilmService } from './services/film.service';
-import { RatingComponent } from './components/rating/rating.component';
-import { FilmsComponent } from './components/films/films.component';
-import { FilmListItemTagComponent } from './components/film-list-item-tag/film-list-item-tag.component';
+import {FilmService} from './services/film.service';
+import {RatingComponent} from './components/rating/rating.component';
+import {FilmsComponent} from './components/films/films.component';
+import {FilmListItemTagComponent} from './components/film-list-item-tag/film-list-item-tag.component';
 import {HttpClientModule} from '@angular/common/http';
 import {HamburgerMenuComponent} from './components/hamburger-menu/hamburger-menu.component';
 import {AddMovieComponent} from './components/add-movie/add-movie.component';
 import {AppRoutingModule} from './routers/app-routing.module';
 import {ThumbsUpComponent} from './components/thumbs-up/thumbs-up.component';
 import {ThumbsDownComponent} from './components/thumbs-down/thumbs-down.component';
+import {LoginComponent} from './components/login/login.component';
+import {StorageService} from "./services/storage.service";
+import { CanActivateComponent } from './components/can-activate/can-activate.component';
 
 @NgModule({
-  declarations: [
-	AppComponent,
-	NavbarComponent,
-	FilmListComponent,
-	FilmListItemComponent,
-	FilmDetailComponent,
-	AboutComponent,
-	RatingComponent,
-	FilmsComponent,
-	FilmListItemTagComponent,
-	AddMovieComponent,
-	HamburgerMenuComponent,
-	ThumbsDownComponent,
-	ThumbsUpComponent
-  ],
-  imports: [
-	BrowserModule,
-	HttpClientModule,
-	Material,
-	AppRoutingModule,
-	StoreModule.forRoot({ films: filmsReducer }),
-	StoreDevtoolsModule.instrument()
-  ],
-  providers: [FilmService],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavbarComponent,
+		FilmListComponent,
+		FilmListItemComponent,
+		FilmDetailComponent,
+		AboutComponent,
+		RatingComponent,
+		FilmsComponent,
+		FilmListItemTagComponent,
+		AddMovieComponent,
+		HamburgerMenuComponent,
+		ThumbsDownComponent,
+		ThumbsUpComponent,
+		LoginComponent
+	],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		Material,
+		FormsModule,
+		ReactiveFormsModule,
+		AppRoutingModule,
+		StoreModule.forRoot({films: filmsReducer}),
+		StoreDevtoolsModule.instrument()
+	],
+	providers: [FilmService, StorageService, CanActivateComponent],
+	bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,32 +1,40 @@
-import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule, MatProgressSpinnerModule, MatSidenav } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import {NgModule} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule, MatProgressSpinnerModule, MatSidenav, MatFormFieldModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatChipsModule} from '@angular/material/chips';
+import {MatInputModule} from '@angular/material/input';
+import {MatNativeDateModule} from '@angular/material';
 
 const MATERIAL_MODULES = [
-  MatCardModule,
-  MatDialogModule,
-  BrowserAnimationsModule,
-  MatButtonModule,
-  MatIconModule,
-  MatToolbarModule,
-  MatListModule,
-  MatTooltipModule,
-  MatProgressSpinnerModule,
-  MatSidenavModule,
-  MatChipsModule
+	MatCardModule,
+	MatDialogModule,
+	BrowserAnimationsModule,
+	MatButtonModule,
+	MatIconModule,
+	MatToolbarModule,
+	MatListModule,
+	MatTooltipModule,
+	MatProgressSpinnerModule,
+	MatSidenavModule,
+	MatChipsModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatDatepickerModule,
+	MatNativeDateModule
 ];
 
 @NgModule({
-  imports: MATERIAL_MODULES,
-  exports: MATERIAL_MODULES,
-  declarations: []
+	imports: MATERIAL_MODULES,
+	exports: MATERIAL_MODULES,
+	declarations: []
 })
-export class Material { }
+export class Material {
+}
