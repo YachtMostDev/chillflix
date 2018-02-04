47f9eb347e4049bde026432f07c54c98cc98a7a9
Gorgony <n.j.vannispen@gmail.com>
2018-02-04 16:06:09 +0100

    Update app-routing.module.ts
    Removed duplicate routing with the wrong component

--- src/app/routers/app-routing.module.ts
+++ src/app/routers/app-routing.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FilmDetailComponent } from '../components/film-detail/film-detail.component';
 import {FilmsComponent} from '../components/films/films.component';
-import { SearchMenuComponent } from '../components/search-menu/search-menu.component';
 
 const routes: Routes = [
 	{ path: 'films/:id', component: FilmDetailComponent },
 	{ path: 'films', component: FilmsComponent },
 	{ path: 'about', component: AboutComponent },
 	{ path: 'home', component: FilmsComponent },
-	{ path: 'films/:id', component: SearchMenuComponent },
 	{ path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
