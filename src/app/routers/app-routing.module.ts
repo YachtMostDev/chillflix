1ce2962452108af17866713a837a51402320b281
Unknown <kayvanbree@gmail.com>
2018-02-06 18:41:09 +0100

    Added video page + routing + routerLink from details component

--- src/app/routers/app-routing.module.ts
+++ src/app/routers/app-routing.module.ts
@@ -1,13 +1,13 @@
 import {AboutComponent} from './../components/about/about.component';
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {FilmDetailComponent} from '../components/film-detail/film-detail.component';
 import {FilmsComponent} from '../components/films/films.component';
 import {LoginComponent} from '../components/login/login.component';
 import {CanActivateComponent} from "../components/can-activate/can-activate.component";
+import {VideoPageComponent} from '../components/video-page/video-page.component';
 
 const routes: Routes = [
-	{path: 'films/:id', component: FilmDetailComponent, canActivate: [CanActivateComponent]},
+	{path: 'films/:id', component: VideoPageComponent, canActivate: [CanActivateComponent]},
 	{path: 'films', component: FilmsComponent, canActivate: [CanActivateComponent]},
 	{path: 'about', component: AboutComponent, canActivate: [CanActivateComponent]},
 	{path: 'home', component: FilmsComponent, canActivate: [CanActivateComponent]},
