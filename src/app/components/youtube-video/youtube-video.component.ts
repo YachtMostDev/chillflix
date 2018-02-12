594c7413450d655e36d3b65776f8cc47ed5939f4
Sander van Rijswijk <sandervanrijswijk@gmail.com>
2018-02-12 16:03:12 +0100

    youtube-video is modified to allow interaction with the iFrame
    Breaking several thing in the process, so some reworking of older files will be needed for all this to work

--- src/app/components/youtube-video/youtube-video.component.ts
+++ src/app/components/youtube-video/youtube-video.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
@@ -7,11 +7,53 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./youtube-video.component.css']
 })
 export class YoutubeVideoComponent {
-  @Input() url;
+  @Input() url : string;
+  public player;
 
   constructor(private sanitizer: DomSanitizer) { }
 
   youtubeURL = () => {
-	return this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
+
+  ngAfterViewInit() {
+    const doc = (<any>window).document;
+    let playerApiScript = doc.createElement('script');
+    playerApiScript.type = 'text/javascript';
+    playerApiScript.src = 'https://www.youtube.com/iframe_api';
+    doc.body.appendChild(playerApiScript);
+  }
+
+  ngOnInit() {
+    (<any>window).onYouTubeIframeAPIReady = () => {
+      this.playVideo();
+    };
+  }
+
+  ngOnDestroy() {
+    console.log(this.player.getCurrentTime());
+  }
+
+  playVideo(){
+    console.log("exodus");
+      this.player = new (<any>window).YT.Player('ytPlayer', {
+        videoId: (this.url).substring(30),
+        events: {
+          'onReady': () => {
+            // go to a specific point in the video, paramter is in seconds
+            if (true){
+            }
+            else {
+              this.player.seekTo(42);
+            }
+            console.log("ready");
+          },
+          'onStateChange': () => {
+            console.log(this.player);
+            // get the current time in the video in seconds
+            console.log(this.player.getCurrentTime());
+          }
+        }
+      });
   }
 }
